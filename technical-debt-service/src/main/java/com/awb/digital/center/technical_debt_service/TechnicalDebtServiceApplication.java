c28c5a2e2c2e39afb76b2e46f2126f21ea89f826
Lazrak-chaymae <cho66el@gmail.com>
2024-06-30 15:04:31 +0100

    Add Spring Boot Folder for Technical Debt service

---
+++ technical-debt-service/src/main/java/com/awb/digital/center/technical_debt_service/TechnicalDebtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.awb.digital.center.technical_debt_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalDebtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalDebtServiceApplication.class, args);
+	}
+
+}
---
+++ technical-debt-service/src/test/java/com/awb/digital/center/technical_debt_service/TechnicalDebtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awb.digital.center.technical_debt_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalDebtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
