333d180a4e806982ad6bdae82110b4c3d1c8db7d
Lazrak-chaymae <cho66el@gmail.com>
2024-07-01 16:43:44 +0100

    Add some component

--- Front/awb-vision/src/App.jsx
+++ Front/awb-vision/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AddProjectComponent from './components/AddProjectComponent'
+import DashboardConstComponent from './components/DashboardConstComponent'
+import DashboardLcmComponent from './components/DashboardLcmComponent'
+import DependancyComponent from './components/DependancyComponent'
+import DetteTechComponent from './components/DetteTechComponent'
+import HeaderComponent from './components/HeaderComponent'
+import KPIProdComponent from './components/KPIProdComponent'
+import ProjectComponent from './components/ProjectComponent'
+import ReleaseComponent from './components/ReleaseComponent'
+import SupportComponent from './components/SupportComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12'>
+            <HeaderComponent />
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-4'>menu</div>
+          <div className='col-8'><ProjectComponent /></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
