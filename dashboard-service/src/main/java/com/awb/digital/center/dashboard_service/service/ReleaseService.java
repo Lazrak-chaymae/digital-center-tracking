8e5d7f4c1bfabd9852b66a5b9b48e4c877e964a7
Lazrak-chaymae <cho66el@gmail.com>
2024-07-07 03:15:43 +0100

    configure route for dashboard service

--- dashboard-service/src/main/java/com/awb/digital/center/dashboard_service/service/ReleaseService.java
+++ dashboard-service/src/main/java/com/awb/digital/center/dashboard_service/service/ReleaseService.java
@@ -5,6 +5,7 @@ import com.awb.digital.center.dashboard_service.dto.ReleaseDto;
 import java.util.List;
 
 public interface ReleaseService {
+
     ReleaseDto createRelease(ReleaseDto releaseDto);
     List<ReleaseDto> getAllRelease();
     ReleaseDto updateRelease(Long id, ReleaseDto releaseDto);
