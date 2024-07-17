44360b31a58cc26b2845c33e856a9898443a010f
Lazrak-chaymae <cho66el@gmail.com>
2024-07-17 09:41:20 +0100

    ajouter route of the squad api to api gateway and handle the under construction project front side

--- awsVision/src/services/DashboardEnConst.js
+++ awsVision/src/services/DashboardEnConst.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const REST_API_BASE_URL = 'http://localhost:8765/api/status/En construction/squad';
+const REST_API_BASE_URL = 'http://localhost:8765/api/projects/under-construction/status/EnConstruction/squad';
 
 export const listProjects = (squadId) => axios.get(REST_API_BASE_URL + '/' + squadId);
 export const listSquads = () => axios.get('http://localhost:8765/api/squads');
